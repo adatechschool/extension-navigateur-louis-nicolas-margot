c035d39b2c543320a4e4e9623b5d3b9d8f6034d9
LouisCoutel <louiscoutel75@gmail.com>
2023-09-07 15:04:02 +0200

    liste catégories et appel api fetch buggé

--- domainInfos/providers/meaningCloud.js
+++ domainInfos/providers/meaningCloud.js
@@ -1,38 +1,30 @@
 // utilitaire de requêtes API
-import axios from "axios";
 
 // "classe", càd une super-fonction/objet qui a ses propres méthodes
 class MCInfos {
     // le constructeur prends les données qu'on lui file en paramètre
-    constructor(urlOfPage, mCkey) {
+    constructor(urlOfPage, mcKey, lang) {
         this.url = urlOfPage
-        this.key = mCkey
+        this.key = mcKey
+        this.lang = lang
 
         // l'API exige des données en format 'formulaire', tout ce qui suit remplace enfait les headers d'une requête classique
         this.form = new FormData();
         this.form.append('url', this.url);
-        this.form.append('Content-Type', 'multipart/form-data');
         this.form.append('key', this.key);
         this.form.append('verbose', 'y');
         this.form.append('expand_hierarchy', 'p');
-        this.form.append('model', 'IPTC_fr');
-        this.form.append('of', 'json')
+        this.form.append('model', `IPTC_${this.lang}`);
     }
 
     // méthode pour lancer la requête
     getMCInfos() {
         // on 'return' ici pour éviter des problèmes de async dans script.js (c'est ça que script.js récupère quand on fait 'infos.getMCinfos()')
-        return axios.post('https://api.meaningcloud.com/class-2.0', this.form)
-            .then((data) => {
-                this.data = data.data.category_list
-                // return final
-                return this.data
+        fetch('https://api.meaningcloud.com/class-2.0', { method: 'post', body: this.form, redirect: 'follow' })
+            .then((response) => {
+                console.log(response)
+                debugger
             })
-            .catch((error) => {
-                // en cas d’échec de la requête
-                console.log(error);
-            })
-
     }
 }
 
--- script.js
+++ script.js
@@ -2,19 +2,37 @@
 import MCInfos from './domainInfos/providers/meaningCloud.js'
 
 // récupére ma clé personnelle d'API
-import mCkey from './domainInfos/providers/key.js'
+import mcKey from './domainInfos/providers/key.js'
 
-const categories = {}
-//crée un objet qui pourra faire l'appel d'API à partir de l'url qu'on lui donne
-async function getCategories() {
-    let infos = new MCInfos('https://wesodonnell.medium.com/ukraine-now-using-cardboard-drones-to-devastate-russian-airfields-a344bb7b9b14', mCkey)
+import catEn from './domainInfos/categoriesLists/catEn.js'
+import catFr from './domainInfos/categoriesLists/catFr.js'
 
-    // lance l'appel d'API et attends la réponse
-    const categories = await infos.getMCInfos()
-    console.log(categories)
+const categories = {
+    categoriesEn: catEn.conceptSet.map(item => item.prefLabel),
+    categoriesFr: catFr.conceptSet.map(item => item.prefLabel)
 }
 
-getCategories()
+//console.log(categories)
+
+
+async function getPageCategories(url, key, lang) {
+    //crée un objet qui pourra faire l'appel d'API à partir de l'url qu'on lui donne
+    let infos = new MCInfos(url, key, lang)
+    // lance l'appel d'API et attends la réponse
+    return infos.getMCInfos()
+
+}
+
+  chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
+      if (changeInfo.status == 'complete') {
+          let currentTabUrl = tab.url
+          console.log(currentTabUrl)
+          let currentPageCategories = getPageCategories(currentTabUrl, mcKey, 'fr')
+          console.log(currentPageCategories)
+      }
+  })
+
+getPageCategories('https://www.jeuxvideo.com/news/1795111/starfield-pc-est-offert-si-vous-achetez-une-souris-dans-cette-grande-enseigne.htm', mcKey, 'fr')
 
 
 
