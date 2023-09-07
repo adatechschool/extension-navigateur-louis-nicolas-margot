06c25d4fb8f2ce6736ad7a234a2bb5e06aa62d54
LouisCoutel <louiscoutel75@gmail.com>
2023-09-07 16:12:46 +0200

    catEn marche, catFr... pas les bonnes cat, req API marche (double .then)

--- domainInfos/providers/meaningCloud.js
+++ domainInfos/providers/meaningCloud.js
@@ -13,17 +13,20 @@ class MCInfos {
         this.form.append('url', this.url);
         this.form.append('key', this.key);
         this.form.append('verbose', 'y');
-        this.form.append('expand_hierarchy', 'p');
+        this.form.append('expand_hierarchy', 'p')
+        this.form.append('of', 'json');
         this.form.append('model', `IPTC_${this.lang}`);
     }
 
     // méthode pour lancer la requête
     getMCInfos() {
-        // on 'return' ici pour éviter des problèmes de async dans script.js (c'est ça que script.js récupère quand on fait 'infos.getMCinfos()')
+        // on 'return' ici pour éviter des problèmes de async dans background.js (c'est ça que background.js récupère quand on fait 'infos.getMCinfos()')
         fetch('https://api.meaningcloud.com/class-2.0', { method: 'post', body: this.form, redirect: 'follow' })
             .then((response) => {
-                console.log(response)
-                debugger
+                return response
+            })
+            .then((responseJSON) => {
+                console.log(responseJSON)
             })
     }
 }
