bada31c26b470cd68ec84501754d088df0f4400c
LouisCoutel <louiscoutel75@gmail.com>
2023-09-12 14:51:35 +0200

    message négatif quand le site est sur blacklist

--- background.js
+++ background.js
@@ -3,18 +3,41 @@ import MCInfos from './domainInfos/providers/meaningCloud.js'
 // récupére ma clé personnelle d'API
 import mcKey from './domainInfos/providers/key.js'
 
+// objets js qui contiennent toutes les catégories
 import catEn from './domainInfos/categoriesLists/catEn.js'
 import catFr from './domainInfos/categoriesLists/catFr.js'
 
+// liste noire de sites
 import blacklist from './blacklist.js'
 
-console.log(blacklist)
+function parseCatFr() {
+    // tableau vide
+    let arr = []
+    for (let i = 1; i < 10; i++) {
+        // on se base sur les "qcode" qui vont de 0100... à 16000..
+        arr.push(`0${i}000000`)
+    }
+    for (let i = 10; i < 17; i++) {
+        arr.push(`${i}000000`)
+    }
+    let filtered = catFr.conceptSet.filter(item =>
+        arr.some(el => item.qcode.includes(el))
+    )
+    console.log(filtered)
+    return filtered.map(item => item.prefLabel)
+}
 
+// 16 principales catégories en anglais et en français
 const categories = {
+    // on prends les 16 premières elles sont dans l'ordre
     categoriesEn: catEn.conceptSet.slice(0, 16).map(item => item.prefLabel),
+    //pas dans l'ordre donc un peu plus compliqué
     categoriesFr: parseCatFr()
 }
 
+console.log(categories)
+
+// numéros catégories "bien" présélectionnées
 let goodCat = [0, 3, 4, 5, 6, 10, 12, 13];
 const goodCategories = [];
 goodCat.forEach(el => goodCategories.push(categories.categoriesEn[el]));
@@ -29,60 +52,51 @@ badCat.forEach(el => badCategories.push(categories.categoriesFr[el]));
 
 console.log(badCategories);
 
-
-function parseCatFr() {
-    let arr = []
-    for (let i = 1; i < 10; i++) {
-        arr.push(`0${i}000000`)
-    }
-    for (let i = 10; i < 17; i++) {
-        arr.push(`${i}000000`)
-    }
-    let filtered = catFr.conceptSet.filter(item =>
-        arr.some(el => item.qcode.includes(el))
-    )
-    console.log(filtered)
-    return filtered.map(item => item.prefLabel)
-}
-
-
-console.log(categories)
-
-
 async function getPageCategories(url, key, lang) {
     //crée un objet qui pourra faire l'appel d'API à partir de l'url qu'on lui donne
     let infos = new MCInfos(url, key, lang)
     // lance l'appel d'API et attends la réponse
     let cats = await infos.getMCInfos()
-    console.log(cats)
     return cats
 }
-let tabCats
+
+function alertMessage() {
+    alert('Nan mais ça va pas ?')
+}
+
+function niceMessage() {
+    alert("c'est bien, continue comme ça")
+}
+
+// lorsqu'un onglet est ouvert ou rafraichi
 // eslint-disable-next-line no-undef
 chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
+    // si il est bien chargé
     if (changeInfo.status == 'complete') {
+        // on récupère la langue
         // eslint-disable-next-line no-undef
         let lang = await chrome.tabs.detectLanguage(tabId)
-        console.log(lang)
+
+        //on récupère l'url
         let currentTabUrl = tab.url
         console.log(currentTabUrl)
-        tabCats = getPageCategories(currentTabUrl, mcKey, lang);
+
+        // si c'est sur la liste noire
+        if (blacklist.some(el => currentTabUrl.includes(el))) {
+            // envoie un message d'alerte
+            // eslint-disable-next-line no-undef
+            chrome.scripting.executeScript({
+                target: { tabId: tabId },
+                func: alertMessage
+            })
+                .then(() => console.log('script'))
+        } else {
+            // sinon, fait un appel d'api pour voir quel type de contenu c'est
+            let tabCats = getPageCategories(currentTabUrl, mcKey, lang);
+            // si ça matche avec badCategories
+        }
     }
 })
-
-chrome.action.onClicked.addListener((tab) => {
-    // const response =
-    //     // eslint-disable-next-line no-undef
-    //     await chrome.runtime.sendMessage({ categories: tabCats });
-    // // do something with response here, not outside the function
-    // console.log(response)
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ['displayCats.js']
-    })
-        .then(() => console.log('script'))
-});
-// eslint-disable-next-line no-undef
 
 
 
--- main.js
+++ main.js
@@ -2,17 +2,31 @@ document.addEventListener("DOMContentLoaded", function () {  // Sert à attendre
     const sliderMode = document.getElementById('checkbox');
     const label = document.querySelector('.mode');
 
-    sliderMode.addEventListener("change", () => { 
+    sliderMode.addEventListener("change", () => {
+        chrome.permissions.request({
+            permissions: ["https://*/*", "http://*/*"],
+        }, (granted) => {
+            // The callback argument will be true if the user granted the permissions.
+            if (granted) {
+                console.log('granted')
+            } else {
+                console.log('not granted')
+            }
+        });
+
         if (sliderMode.checked) {     // Activation de l'extension.
             console.log("Active");
             // On récupère l'URL du site que l'utilisateur est en train d'utiliser.
         } else {  // Désactivation de l'extension.
             console.log("Desactive");
-        } 
-       label.textContent = sliderMode.checked ? "On" : "Off"; // Fonction qui permet de changer la position du bouton.
+        }
+        label.textContent = sliderMode.checked ? "On" : "Off"; // Fonction qui permet de changer la position du bouton.
     });
 
     label.addEventListener("click", () => { // Fonction qui permet de changer "On" à "Off" et vice-versa.
+        // Permissions must be requested from inside a user gesture, like a button's
+        // click handler.
+
         sliderMode.checked = !sliderMode.checked;
         label.textContent = sliderMode.checked ? "On" : "Off";
     })
