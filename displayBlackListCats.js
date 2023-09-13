db76c6b2f2d4c34894c9b3814c32d4c710984475
MargotHuet <margothuet4@gmail.com>
2023-09-13 16:46:37 +0200

    Ajout de setTimeOut() sur les fonctions displaysCats

--- displayBlackListCats.js
+++ displayBlackListCats.js
@@ -3,3 +3,6 @@ chat.id = 'box'
 chat.setAttribute('style', 'position: fixed; top: 30%; left: 30%; width: 50%;, height: 50%; z-index: 999;  ')
 chat.innerHTML = '<img id="chat" src="https://imagizer.imageshack.com/img923/5786/4thk8Y.png"></img>'
 document.body.appendChild(chat)
+setTimeout(() => {
+    document.body.removeChild(chat)
+}, 3000);
